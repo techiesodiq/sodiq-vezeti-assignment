b973032d4aefbb3f2ccbb745cf68e230c9fb0b5e
Sodiq <sodiq.akanmu001@gmail.com>
2020-09-18 10:40:52 +0100

    update email

---
+++ build/static/js/63.cec8ef6f.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpsodiq-vezeti-assignment"]=this["webpackJsonpsodiq-vezeti-assignment"]||[]).push([[63],{2127:function(e,n,t){"use strict";t.r(n);var a=t(0),l=t.n(a),s=t(332),i=Object(a.lazy)((function(){return t.e(49).then(t.bind(null,2084))}));n.default=function(){return l.a.createElement(s.a,{title:"API Calls"},l.a.createElement(a.Suspense,{fallback:null},l.a.createElement(i,null)))}}}]);
+//# sourceMappingURL=63.cec8ef6f.chunk.js.map
